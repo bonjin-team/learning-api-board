8571095eb0a96f5af31b13de313c987e3787c662
bonjin-app <bonjin.app@gmail.com>
2021-09-13 11:18:19 +0900

    Init Project

---
+++ src/main/java/kr/co/bonjin/learningapiboard/LearningApiBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.learningapiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApiBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningApiBoardApplication.class, args);
+    }
+
+}
