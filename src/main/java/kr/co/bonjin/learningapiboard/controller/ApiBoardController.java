d7b0b60757ca4a6da0c250eb54355eaa414dc122
bonjin-app <bonjin.app@gmail.com>
2021-09-13 11:50:09 +0900

    Update ApiController: BoardService DI  추가.

--- src/main/java/kr/co/bonjin/learningapiboard/controller/ApiBoardController.java
+++ src/main/java/kr/co/bonjin/learningapiboard/controller/ApiBoardController.java
@@ -4,6 +4,7 @@ import kr.co.bonjin.learningapiboard.exception.ApiError;
 import kr.co.bonjin.learningapiboard.exception.ApiException;
 import kr.co.bonjin.learningapiboard.response.ApiDataResponse;
 import kr.co.bonjin.learningapiboard.response.ApiResponse;
+import kr.co.bonjin.learningapiboard.service.BoardService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/board")
 public class ApiBoardController {
+
+    private BoardService boardService;
+
+    public ApiBoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
     @GetMapping
     public ApiResponse getBoardList() {
